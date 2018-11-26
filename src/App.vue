fc39c33a75536703c408732dce4e2fe7a32825d2
NikulinIlya <ilia.nikulin@gmail.com>
2018-11-26 18:29:56 +0300

    remove rb pattern

--- src/App.vue
+++ src/App.vue
@@ -28,13 +28,7 @@
                        v-model="activePlan">
       </selectable-plan>
     </div>
-    <input type="radio" id="one" value="One" v-model="picked">
-    <label for="one">One</label>
-    <br>
-    <input type="radio" id="two" value="Two" v-model="picked">
-    <label for="two">Two</label>
-    <br>
-    <span>Picked: {{ picked }}</span>
+
   </div>
 </template>
 
